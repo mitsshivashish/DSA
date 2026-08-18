1bb7421c135f18b4667917e212737744213ac8ab
mitsshivashish <Shivashishtiwari18@gmail.com>
2026-08-18 23:01:46 +0530

    Removed duplicates in an array

---
+++ Day 04/remove_duplicates.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int size = nums.size();
+        int j = 0;
+
+        for (int i = 1; i < size; i++)
+        {
+            if (nums[i] != nums[j])
+            {
+                j++;
+                nums[j] = nums[i];
+            } 
+        }
+        return (j + 1);
+    }
+};
